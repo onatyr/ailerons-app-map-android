a5bfb4cb4a5928b29fcf5cb73a84d2daa23f7587
onatyr <onat.rigault@gmail.com>
2024-01-23 17:05:33 +0100

    feature/icon shark and ray

--- app/src/main/java/fr/onat68/aileronsappmapandroid/species/SpeciesComposables.kt
+++ app/src/main/java/fr/onat68/aileronsappmapandroid/species/SpeciesComposables.kt
@@ -2,15 +2,17 @@ package fr.onat68.aileronsappmapandroid.species
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.NavController
+import fr.onat68.aileronsappmapandroid.R
 import fr.onat68.aileronsappmapandroid.individual.Individual
 
 @Composable
@@ -22,18 +24,26 @@ fun IndividualsList(individualsList: List<Individual>, navController: NavControl
         }
     }
 }
+
 @Composable
-fun IndividualItem(individual: Individual, navController: NavController){
+fun IndividualItem(individual: Individual, navController: NavController) {
+    val sharkImageBitmap = LocalContext.current.getDrawable(R.drawable.ic_shark)!!.toBitmap().asImageBitmap()
+    val rayImageBitmap = LocalContext.current.getDrawable(R.drawable.ic_ray_manta)!!.toBitmap().asImageBitmap()
+    val iconList = listOf(sharkImageBitmap, rayImageBitmap)
+
     ListItem(
         headlineContent = { Text(individual.individualName) },
         supportingContent = {
             Text(individual.binomialName)
         },
         leadingContent = {
-            Icon(
-                Icons.Filled.Favorite,
-                contentDescription = "Localized description",
-            )
+                         Icon(
+                             bitmap = iconList[individual.icon-1], contentDescription = "Shark Icon"
+                         )
+//            Icon(
+//                Icons.Filled.Favorite,
+//                contentDescription = "Localized description",
+//            )
         },
         trailingContent = { Text("meta") },
         modifier = Modifier.clickable { navController.navigate("individualSheet/${individual.individualRecordId}") }
