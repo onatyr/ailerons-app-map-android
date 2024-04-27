6b794cfb548f7f78890c627def1b9369d65b4dc3
onatyr <onat.rigault@gail.com>
2024-04-27 19:59:19 +0200

    FavoritesViewModel modified to be conform to the repository

--- app/src/main/java/fr/onat68/aileronsappmapandroid/favorites/FavoritesViewModel.kt
+++ app/src/main/java/fr/onat68/aileronsappmapandroid/favorites/FavoritesViewModel.kt
@@ -3,6 +3,8 @@ package fr.onat68.aileronsappmapandroid.favorites
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import fr.onat68.aileronsappmapandroid.data.entities.Favorite
+import fr.onat68.aileronsappmapandroid.data.entities.Individual
+import fr.onat68.aileronsappmapandroid.data.entities.IndividualDAO
 import fr.onat68.aileronsappmapandroid.data.entities.IndividualDTO
 import fr.onat68.aileronsappmapandroid.data.repositories.IndividualRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,37 +12,20 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class FavoritesViewModel @Inject constructor(private val individualRepository: IndividualRepository)
-: ViewModel() {
+class FavoritesViewModel @Inject constructor(private val individualRepository: IndividualRepository) :
+    ViewModel() {
 
-    val favoritesList: Flow<List<Favorite>> = individualRepository.getListFavorite()
+    val favoritesList: Flow<List<Individual>> = individualRepository.getListFavorite()
 
+    private fun addFav(individualId: Int) = individualRepository.addFavorite(individualId)
 
-    private fun addFav(individualId: Int) {
+    private fun deleteFav(individualId: Int) = individualRepository.removeFavorite(individualId)
 
-        viewModelScope.launch {
-            val newFavorite = Favorite(individualId)
-            database.FavoriteDao().insert(newFavorite)
-            getAll()
-        }
-    }
-
-    private fun deleteFav(individualId: Int) {
-
-        viewModelScope.launch {
-            database.FavoriteDao().delete(individualId)
-            getAll()
-        }
-    }
-
-    fun changeFav(individualId: Int) {
-
-        viewModelScope.launch {
-            if (database.FavoriteDao().getAll().map { it.individualId }.contains(individualId)) {
-                deleteFav(individualId)
-            } else {
-                addFav(individualId)
-            }
+    fun changeFav(individual: Individual) {
+        if (individual.isFavorite) {
+            deleteFav(individual.id)
+        } else {
+            addFav(individual.id)
         }
     }
 }
